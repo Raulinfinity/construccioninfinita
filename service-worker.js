7194f77fbd758d3e2de82d75a95eb5a9ee0e4eec
Raulinfinity <raulramirezmacias14@gmail.com>
2025-04-27 11:54:00 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,11 @@
-// Service Worker Básico
-self.addEventListener('install', e => {self.skipWaiting();});
+self.addEventListener('install', function(event) {
+  console.log('Service Worker instalado correctamente.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activado y listo para controlar la app.');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Interceptando solicitud a:', event.request.url);
+});
