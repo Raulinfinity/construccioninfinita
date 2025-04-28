fb827b2474b94234d36bdb6d1dffa9200237f97c
Raulinfinity <raulramirezmacias14@gmail.com>
2025-04-27 23:23:46 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,14 +5,12 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('Service Worker activado.');
-  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
